2e37cc0e15cd50ac8fdb0014c1b4460c99713119
zdl <zdl@666.om>
2017-08-22 12:59:28 +0800

    奇葩

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,6 +6,7 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("args = [ 1111]");
+        System.out.println("args = [ 112222222221]");
     }
 
 }
